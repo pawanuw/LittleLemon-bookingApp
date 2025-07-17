795594d5a5b31441fd99a4364a8fa36e5312dc50
Pawan Weerasuriya <uwpawan@gmail.com>
2025-07-18 02:38:27 +0530

    [dev] models registration had an error and it fixed, models were edited. then migrated.

--- LittleLemon/Restaurant/admin.py
+++ LittleLemon/Restaurant/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from LittleLemon.Restaurant.models import Menu
-from LittleLemon.Restaurant.models import Booking
+from .models import Menu
+from .models import Booking
 
 # Register your models here.
 admin.site.register(Menu)

0d01783febd6269a85b12b7684e9935cc1834dc7
Pawan Weerasuriya <uwpawan@gmail.com>
2025-07-18 02:23:44 +0530

    [dev] models were registered in admin.py

--- LittleLemon/Restaurant/admin.py
+++ LittleLemon/Restaurant/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from LittleLemon.Restaurant.models import Menu
+from LittleLemon.Restaurant.models import Booking
+
 # Register your models here.
+admin.site.register(Menu)
+admin.site.register(Booking)

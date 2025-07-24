63a60cf1184eab981f65af3bc03246851df1df69
Pawan Weerasuriya <uwpawan@gmail.com>
2025-07-24 12:26:52 +0530

    [dev] authtoken is added. settings.py editted. models too upgraded, also migrated.

--- LittleLemon/Restaurant/models.py
+++ LittleLemon/Restaurant/models.py
@@ -12,7 +12,7 @@ class Booking(models.Model):
 class Menu(models.Model):
     title = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    inventory = models.IntegerField(max_length=5)
+    inventory = models.SmallIntegerField()
 
     def __str__(self):
         return self.title

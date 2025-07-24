2c20e9597258223229bf4d0b1c3ad77b7a8fc9e5
Pawan Weerasuriya <uwpawan@gmail.com>
2025-07-24 13:17:29 +0530

    [dev] menuitems and bookings were secured with the djoser.

--- LittleLemon/Restaurant/urls.py
+++ LittleLemon/Restaurant/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import MenuItemView, SingleMenuItemView, BookingView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('menu/', MenuItemView.as_view(), name='menu-list'),
     path('menu/<int:pk>/', SingleMenuItemView.as_view(), name='menu-detail'),
     path('booking/', BookingView.as_view(), name='booking-list'),
+    path('api-token-auth/', obtain_auth_token),
 ]
--- LittleLemon/Restaurant/views.py
+++ LittleLemon/Restaurant/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer, UserSerializer
 from rest_framework import viewsets, permissions
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -13,6 +14,7 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 class MenuItemView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
@@ -21,5 +23,6 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = MenuSerializer
 
 class BookingView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
